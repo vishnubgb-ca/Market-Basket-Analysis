6ff9b3589568324a0c65da383281f652ab18a2e3
Vishnu_B_G_Bharadwaja <146937608+vishnubgb-ca@users.noreply.github.com>
2023-10-21 17:49:11 +0530

    Update loading_data.py

--- loading_data.py
+++ loading_data.py
@@ -1,7 +1,7 @@
 import  pandas as pd
 
 def loading_data():
-    data = pd.read_csv('./UCI_Credit_Card.csv')
+    data = pd.read_csv("Market_Basket_Optimisation.csv",header=None)
     return data
 
 loading_data()
