06293d33d7f410d178330b349fb9606cde75804d
Vishnu_B_G_Bharadwaja <146937608+vishnubgb-ca@users.noreply.github.com>
2023-12-02 17:16:23 +0530

    Update datavisualization.py

--- datavisualization.py
+++ datavisualization.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from data_preprocessing import data_preprocess
 import seaborn as sns
+import plotly.express as px
 import squarify
 import pandas as pd
 
@@ -12,35 +13,60 @@ def data_visualization():
     # Select the top 10 unique values and their counts
     unique_values = value_counts.index[:10]  # Get the top 10 unique values
     value_counts_top_10 = value_counts.values[:10]  # Get the counts for the top 10 unique values
-    
+
+
+
+
+    fig = px.bar(x=unique_values, y=value_counts_top_10, labels={'x': 'Products', 'y': 'Counts'},
+                 title='Top 10 Products', text=value_counts_top_10)
+    fig.update_layout(template='plotly_dark')
+    #fig.update_layout(plot_bgcolor = "black")
+    fig.update_xaxes(showgrid=False)
+    fig.update_yaxes(showgrid=False)
+    fig.write_image(f"bar.jpg")
     # Create a bar graph
-    plt.figure(figsize=(18,16))
-    plt.bar(unique_values, value_counts_top_10)
-    plt.xlabel("Products")
-    plt.ylabel("Counts")
-    plt.title("Top 10 Products")
-    plt.xticks(rotation=90)
-    plt.show()
+    # plt.figure(figsize=(18,16))
+    # plt.bar(unique_values, value_counts_top_10)
+    # plt.xlabel("Products")
+    # plt.ylabel("Counts")
+    # plt.title("Top 10 Products")
+    # plt.xticks(rotation=90)
+    # plt.show()
 
     top15items = pd.DataFrame(data.stack().value_counts().head(15))
     top15items = top15items.reset_index()
     top15items.columns = ["Itemname","Frequency"]
     labels = top15items["Itemname"]
     sizes = top15items["Frequency"]
-    plt.figure(figsize=(15,13))
-    plt.pie(sizes,labels=labels)
-    plt.show()
+    fig = px.pie(names=labels, values=sizes, title='Pie Chart')
+    fig.update_traces(textposition='inside', textinfo='percent+label')
+    fig.write_image(f"pie.jpg")
+    # plt.figure(figsize=(15,13))
+    # plt.pie(sizes,labels=labels)
+    # plt.show()
     
     top20items = pd.DataFrame(data.stack().value_counts().head(20))
     top20items = top20items.reset_index()
     top20items.columns = ["Itemname","Frequency"]
     labels = top20items["Itemname"]
     sizes = top20items["Frequency"]
-    fig = plt.figure(figsize=(16,10))
-    colors = sns.color_palette("Spectral",20)
-    squarify.plot(sizes, label=labels, color =  colors)
-    plt.title("Top 20 Products")
-    plt.show()
+    df = pd.DataFrame({'labels': labels, 'sizes': sizes})
+    
+    fig = px.treemap(df, path=['labels'], values='sizes',
+                     color_continuous_scale='Spectral', title='Top 20 Products')
+    
+    fig.update_layout(width=800, height=500)
+    fig.update_layout(template='plotly_dark')
+    #fig.update_layout(plot_bgcolor = "black")
+    fig.update_xaxes(showgrid=False)
+    fig.update_yaxes(showgrid=False)
+    fig.write_image(f"square.jpg")
+    
+    # fig = plt.figure(figsize=(16,10))
+    # colors = sns.color_palette("Spectral",20)
+    # squarify.plot(sizes, label=labels, color =  colors)
+    # plt.title("Top 20 Products")
+    # plt.show()
     
     return data
 
